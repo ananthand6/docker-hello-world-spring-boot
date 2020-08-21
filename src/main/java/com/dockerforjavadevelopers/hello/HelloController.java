45def57e157ea378140e258060d1daa4778b5386
ananthand6 <67504897+ananthand6@users.noreply.github.com>
2020-08-21 16:06:54 +0530

    Update HelloController.java

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-public class HelloController {
+//public class HelloController {
     
-    @RequestMapping("/")
-    public String index() {
-        return "Hello World, This is a Message from ANANTHAN \n";
+   // @RequestMapping("/")
+   // public String index() {
+      //  return "Hello World, This is a Message from ANANTHAN \n";
     }
     
 }
